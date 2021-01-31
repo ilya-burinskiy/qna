0d00ea68a05763f7047dc9730639077634b81167
Ilya <burinskiy992@gmail/com>
2021-01-31 12:48:44 +0300

    Question have_many association test

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  # let(:name) { 'name' }
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
+  it { should have_many(:answers) }
 end
