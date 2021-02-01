4557c826911fb44f652949074a26b89be361f980
Ilya <burinskiy992@gmail/com>
2021-02-01 14:41:22 +0300

    Changed schema

--- db/migrate/20210131093147_change_columns_null_to_false_in_questions.rb
+++
@@ -1,6 +0,0 @@
-class ChangeColumnsNullToFalseInQuestions < ActiveRecord::Migration[6.1]
-  def change
-    change_column_null :questions, :title, false
-    change_column_null :questions, :body, false
-  end
-end
