e4f89fd6aad331da91e1e801f1debaa07c6ef30c
Ilya <burinskiy992@gmail/com>
2021-02-07 17:18:06 +0300

    Browse questions feature update

--- spec/features/question/browse_spec.rb
+++ spec/features/question/browse_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 feature 'User can view the question and answers to it' do
   given(:user) { create(:user) }
-  given!(:question) { create(:question) }
-  given!(:answer) { create(:answer, question: question) }
+  given!(:question) { create(:question, author: create(:user)) }
+  given!(:answer) { create(:answer, question: question, author: create(:user)) }
 
   scenario 'user views all question answers' do
     visit question_path(question)
