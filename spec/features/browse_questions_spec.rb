2c44a119fe3f83d33fcc43b2c72894bfe3b50ef2
Ilya <burinskiy992@gmail/com>
2021-02-08 18:19:24 +0300

    Answer and Question browse features update

--- spec/features/browse_questions_spec.rb
+++ spec/features/browse_questions_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
 feature 'User can browse questions' do
-  given!(:question) { create(:question) }
+  given!(:questions) { create_list(:question, 3) }
 
   scenario 'any user can browse questions' do
     visit questions_path
 
-    expect(page).to have_content question.title
+    questions.each do |question|
+      expect(page).to have_content question.title
+    end
   end
 end
