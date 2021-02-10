9efbc8757bc4ffce38c831966eccf5123c5d7646
Ilya <burinskiy992@gmail/com>
2021-02-10 11:49:20 +0300

    Using be author in user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,20 +7,17 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
 
-  describe 'User #author?' do
+  describe '#author?' do
     let(:user1) { create(:user) }
     let(:user2) { create(:user) }
     let(:question) { create(:question, author: user1) }
-    let(:answer) { create(:answer, question: question, author: user1) }
 
-    it 'should be author of answer and question' do
-      expect(user1.author?(answer)).to eq true
-      expect(user1.author?(question)).to eq true
+    it 'should be author of question' do
+      expect(user1).to be_author(question)
     end
 
-    it 'should not be author of answer and question' do
-      expect(user2.author?(answer)).to eq false
-      expect(user2.author?(question)).to eq false
+    it 'should not be author of question' do
+      expect(user2).to_not be_author(question)
     end
   end
 end
