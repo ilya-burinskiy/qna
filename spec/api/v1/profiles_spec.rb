01ed1e59ff7dba93c693a1cc3fcafef900059b10
Ilya <burinskiy992@gmail/com>
2021-03-22 17:22:19 +0300

    Profiles list api

--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -39,4 +39,50 @@ describe 'Profiles API', type: :request do
       end
     end
   end
+
+  describe 'GET /api/v1/profiles' do
+    it_behaves_like 'API Authorizable' do
+      let(:api_path) { '/api/v1/profiles/me' }
+      let(:method) { :get }
+    end
+
+    context 'Authorized' do
+      let(:me) { create(:user) }
+      let!(:users) { create_list(:user, 3) }
+      let(:access_token) { create(:access_token, resource_owner_id: me.id) }
+      let(:users_response) { json['users'] }
+
+      before do
+        get '/api/v1/profiles', params: { access_token: access_token.token }, headers: headers
+      end
+
+      it 'returns profiles list of all non authenticated users' do
+        expect(users_response.size).to eq users.size
+      end
+
+      it 'returns all public profile fields of all non authenticated users' do
+        users.each_with_index do |user, i|
+          %w[id email type created_at updated_at].each do |attr|
+            expect(users_response[i][attr]).to eq user.send(attr).as_json
+          end
+        end
+      end
+
+      it 'does not return private profile fields of all non authenticated users' do
+        users.each_with_index do |user, i|
+          %w[password encrypted_password].each do |attr|
+            expect(users_response[i]).to_not have_key(attr)
+          end
+        end
+      end
+
+      it 'does not return profile of authenticated user' do
+        expect(
+          users_response.any? do |user|
+            user['id'] == me.id
+          end
+        ).to be_falsey
+      end
+    end
+  end
 end
